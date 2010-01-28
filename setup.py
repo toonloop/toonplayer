0be48db4a8914127d8d08baa30daeb0314adfa55
Alexandre Quessy <alexandre@quessy.net>
2010-01-27 23:19:33 -0500

    fixes in version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from distutils.core import setup
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 setup(
     name="toonplayer",
--- toonplay/runner.py
+++ toonplay/runner.py
@@ -27,7 +27,7 @@
 """
 Main entry point of the application.
 """
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 import os
 import sys
